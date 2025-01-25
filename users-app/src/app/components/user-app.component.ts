9ae53185b7837df52f249f368353c9d46f9a415a
MatyBene <benedettimaty@gmail.com>
2025-01-25 00:47:59 -0300

    crea estructura base del proyecto

---
+++ users-app/src/app/components/user-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-app',
+  imports: [],
+  templateUrl: './user-app.component.html',
+})
+export class UserAppComponent {
+
+  title: string = 'Listado de usuarios'
+}
