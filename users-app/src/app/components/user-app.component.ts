ab3f4f0f92d6e24789fcb5389f52befda6d7040c
MatyBene <benedettimaty@gmail.com>
2025-01-26 22:05:28 -0300

    crea componente form

--- users-app/src/app/components/user-app.component.ts
+++ users-app/src/app/components/user-app.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user';
 import { UserService } from '../services/user.service';
 import { UserComponent } from './user/user.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @Component({
   selector: 'user-app',
-  imports: [UserComponent],
+  imports: [UserComponent, UserFormComponent],
   templateUrl: './user-app.component.html',
 })
 export class UserAppComponent implements OnInit {
@@ -20,4 +21,7 @@ export class UserAppComponent implements OnInit {
     this.service.findAll().subscribe(users => this.users = users);
   }
   
+  addUser(user: User){
+    this.users = [... this.users, { ... user }];
+  }
 }
